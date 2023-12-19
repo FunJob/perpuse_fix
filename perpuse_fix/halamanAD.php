587b94158c471de9a7149804efed86a2be99af20
funjob <mn9603379@gmail.com>
2023-12-19 20:47:44 +0700

    Delete perpuse_fix directory

--- perpuse_fix/halamanAD.php
+++
@@ -1,5 +0,0 @@
-<?php include 'headerAD.php'; ?>
-<h3 align="center">Selamat Datang di Perpustakaan SMA Negeri 2 Kota Tangerang Selatan</h3>
-
-
-<?php include 'footer.php'; ?>
--- perpuse_fix/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$konek = mysqli_connect('localhost','id21670894_mnfal','JHIi9e998(*jsk','id21670894_perpuse');
-if (!$konek){
-	echo "can't find database named buku_tamu";
-}
-
- ?>
